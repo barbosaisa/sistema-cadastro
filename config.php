41655ace303310bfe45e0ed1ee7388e8c6cbf242
Isabella Barbosa <106408514+barbosaisa@users.noreply.github.com>
2023-11-08 20:23:30 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'sistema_cadastros';
+    
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+?>
